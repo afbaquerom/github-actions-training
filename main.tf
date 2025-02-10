3f915733d9bddd66f52a087f78e724faa3927a21
Andres Felipe Baquero Marulanda <1144033325@Comfandi.com.co>
2025-02-10 11:17:42 -0500

    Primer commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-tf-test-bucket-andres-19287319837892137"
+  acl    = "private"
+tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
